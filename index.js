9085d57596856cb241b7a8e167d9f3be099b4da5
Chris Zempel <chris.zempel@maritz.com>
2018-10-14 07:07:14 -0500

    Adds cors headers

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (_req, res) => {
   res.send(`ಠ_ಠ`);
